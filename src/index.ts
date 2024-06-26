576127eded0fd9f6bd0faa96d5cdacecdbc67bdd
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-06-27 00:40:58 +0300

    new part

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import { connectToMongoDB } from "./database";
+import router from "./createUser"; 
+import SomeModel from "./models/SomeModel";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
+
+
+const port = process.env.PORT || 5000;
+
+async function startServer() {
+  await connectToMongoDB();
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
+}
+
+startServer();
