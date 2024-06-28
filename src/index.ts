51d3c9bafe4ae5a0a480a67321d50d3836797049
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-06-28 23:03:24 +0300

    better

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-import morgan from 'morgan'; 
+import { connectToMongoDB } from "./database";
+import router from "./routes"; 
+import SomeModel from "./models/UserModel";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny')); 
 
-app.get('/', function (req, res) {
-  res.send('hello, world!')
-})
+app.use("/api", router);
+
+app.get("/api/data", async (req: Request, res: Response) => {
+  try {
+    const data = await SomeModel.find(); 
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 const port = process.env.PORT || 5000;
 
+async function startServer() {
+  await connectToMongoDB();
   app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
-   });
+  });
+}
 
+startServer();
