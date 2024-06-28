330b083e842f0c96fa9a6fec4ee71cb411518db9
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-06-28 21:05:45 +0300

    new

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny')); 
 
-
- 
 app.get('/', function (req, res) {
   res.send('hello, world!')
 })
 
 const port = process.env.PORT || 5000;
-
 
   app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
