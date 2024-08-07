efb72ae99f2d9c10aeb13661c1ef9fc671e2ee09
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-08-08 01:43:26 +0300

    update

--- src/tests/models/User/Email.test.ts
+++ src/tests/models/User/Email.test.ts
@@ -1,26 +1,34 @@
+
 import { describe, expect, test } from "@jest/globals";
 import Email from "../../../models/User/Email";
 import ParseException from "../../../exceptions/ParseException";
 
 describe("Email", () => {
   test("can be constructed with valid emails", () => {
-    const validEmails = ["john@gmail.com", "test@example.com", "user.name@domain.co", "user+name@domain.com"];
+    const validEmails = [
+      "john@gmail.com",
+      "test@example.com",
+      "user.name@domain.co",
+      "user+name@domain.com"
+    ];
 
     validEmails.forEach(email => {
       const emailInstance = new Email(email);
-      expect(emailInstance.get()).toBe(email);
+      expect(emailInstance.get()).toBe(email); 
     });
   });
 
-  test("throws an exception for an invalid email", () => {
-    const invalidEmails = ["plainaddress", "@missingusername.com", "username@.com", "invalid-email"];
+  test("throws an exception for invalid emails", () => {
+    const invalidEmails = [
+      "plainaddress",
+      "@missingusername.com",
+      "username@.com",
+      "invalid-email",
+      ""
+    ];
 
     invalidEmails.forEach(email => {
       expect(() => new Email(email)).toThrow(ParseException);
     });
-  });
-
-  test("throws an exception for an empty email", () => {
-    expect(() => new Email("")).toThrow(ParseException);
   });
 });
